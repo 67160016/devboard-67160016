3c9f684e2b7381e96d7251611fad390e90617027
patinya phunphon <67160016@go.buu.ac.th>
2026-03-29 14:36:45 +0700

    task1-4 remake

---
+++ src/components/PostList.jsx
@@ -0,0 +1,28 @@
+import PostCard from "./PostCard";
+import PostCount from "./PostCount";
+
+function PostList({ posts }) {
+  console.log(posts);
+  return (
+    <div>
+      <h2
+        style={{
+          color: "#2d3748",
+          borderBottom: "2px solid #1e40af",
+          paddingBottom: "0.5rem",
+        }}
+      >
+        โพสต์ล่าสุด
+      </h2>
+
+      {/* ✅ ใส่ตรงนี้ */}
+      <PostCount count={posts.length} />
+
+      {posts.map((post) => (
+        <PostCard key={post.id} title={post.title} body={post.body} />
+      ))}
+    </div>
+  );
+}
+
+export default PostList;
